bf4ff32272147cd027d4aeca226e6edb7adbab11
Basheer Khuddus <bkhuddus@onehousing.co.uk>
2020-10-25 23:57:05 +0000

    Adding to DevOps

---
+++ DocTransfer/DocTransfer/LogHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Configuration;
+using System.IO;
+
+namespace DocTransfer
+{
+    class LogHelper
+    {
+        internal static void LogFile(string content)
+        {
+            var path = ConfigurationManager.AppSettings["LogFilePath"]; ;
+            using (var txtFile = File.AppendText(path))
+            {
+                txtFile.WriteLine(DateTime.Now.ToString() + " - " + content);
+            }
+        }
+    }
+}
